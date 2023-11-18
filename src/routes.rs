ddd52cdbc46c20626e54b32c9919bd2ab4ead39e
Sean Craven <sean.craven@advai.co.uk>
2023-11-18 14:21:23 +0000

    feat: htmx start

--- src/routes.rs
+++ src/routes.rs
@@ -1,7 +1,49 @@
-use actix_web::{get, HttpRequest, HttpResponse, Responder};
+use actix_web::{get, HttpResponse, Responder};
+use serde::Serialize;
 use std::fs::read;
+use tera::{self, Context};
 
 #[get("/")]
 async fn index() -> impl Responder {
-    HttpResponse::Ok().body(read("./pages/index.html").expect("failed to get index"))
+    let dates = vec![
+        Date {
+            name: String::from("placeholder"),
+        };
+        5
+    ];
+    HttpResponse::Ok().body(template_load(dates).expect("Templating failed."))
+}
+
+#[derive(Debug, Serialize, Clone)]
+struct Date {
+    name: String,
+}
+
+fn template_load(dates: Vec<Date>) -> anyhow::Result<String> {
+    let mut ctx = Context::new();
+    ctx.insert("dates", &dates);
+    let html = tera::Tera::one_off(
+        std::str::from_utf8(&read("./pages/index.html").expect("File system read failed."))
+            .expect("Failed at utf decoding."),
+        &ctx,
+        true,
+    )?;
+    Ok(html)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_template() -> anyhow::Result<()> {
+        let dates = vec![
+            Date {
+                name: String::from("dave")
+            };
+            5
+        ];
+        assert!(!template_load(dates.clone())?.contains("% for date in dates %"));
+        assert!(template_load(dates)?.contains("dave"));
+        Ok(())
+    }
 }
