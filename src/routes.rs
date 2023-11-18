41868df0b83aae39930ad8e80377c676c28ed097
Sean Craven <sean.craven@advai.co.uk>
2023-11-18 15:14:49 +0000

    feat: adding new like dislike button

--- src/routes.rs
+++ src/routes.rs
@@ -1,22 +1,65 @@
-use actix_web::{get, HttpResponse, Responder};
+use actix_web::{get, post, HttpResponse, Responder};
 use serde::Serialize;
 use std::fs::read;
 use tera::{self, Context};
 
 #[get("/")]
 async fn index() -> impl Responder {
-    let dates = vec![
-        Date {
-            name: String::from("placeholder"),
-        };
-        5
-    ];
+    let dates = vec![Date::new(String::from("placeholder")); 5];
     HttpResponse::Ok().body(template_load(dates).expect("Templating failed."))
 }
 
 #[derive(Debug, Serialize, Clone)]
 struct Date {
     name: String,
+    count: usize,
+}
+impl Date {
+    fn new(name: String) -> Date {
+        Date { name, count: 0 }
+    }
+    fn add(&mut self) {
+        self.count += 1;
+    }
+}
+
+trait Repository {
+    fn add(&mut self, date: Date);
+    fn remove(&mut self, date: Date);
+    fn get(&self) -> Vec<Date>;
+    fn update_count(&mut self, date_name: &str);
+}
+
+struct DateRepo {
+    dates: Vec<Date>,
+}
+impl Repository for DateRepo {
+    fn add(&mut self, date: Date) {
+        self.dates.push(date);
+    }
+    fn get(&self) -> Vec<Date> {
+        self.dates.clone()
+    }
+    fn update_count(&mut self, date_name: &str) {
+        for _date in self.dates.iter_mut() {
+            if &*_date.name == date_name {
+                _date.add();
+                break;
+            };
+        }
+    }
+    fn remove(&mut self, date: Date) {
+        let mut removal_ind = None;
+        for (i, _date) in self.dates.iter().enumerate() {
+            if _date.name == date.name {
+                removal_ind = Some(i);
+                break;
+            }
+        }
+        if let Some(r_ind) = removal_ind {
+            self.dates.remove(r_ind);
+        }
+    }
 }
 
 fn template_load(dates: Vec<Date>) -> anyhow::Result<String> {
@@ -36,14 +79,10 @@ mod tests {
     use super::*;
     #[test]
     fn test_template() -> anyhow::Result<()> {
-        let dates = vec![
-            Date {
-                name: String::from("dave")
-            };
-            5
-        ];
+        let dates = vec![Date::new(String::from("dave")); 5];
         assert!(!template_load(dates.clone())?.contains("% for date in dates %"));
-        assert!(template_load(dates)?.contains("dave"));
+        assert!(template_load(dates.clone())?.contains("dave"));
+        println!("{}", template_load(dates)?);
         Ok(())
     }
 }
