046cb48747a131023b945ce8a1a2ec5f7dfced4e
seancraven <seanpietercraven@gmail.com>
2023-11-26 15:02:45 +0000

    setup

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ mod templating;
 
 use actix_web::web;
 use actix_web::web::ServiceConfig;
-use domain::repository::{AppState, Date, VecRepo};
+use domain::repository::{AppState, VecRepo};
 use routes::dates::dates_service;
 use routes::index::index;
 use shuttle_actix_web::ShuttleActixWeb;
@@ -13,8 +13,8 @@ use std::sync::Mutex;
 #[shuttle_runtime::main]
 async fn main() -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
     let state = AppState::new(Mutex::new(Box::new(VecRepo::new(vec![
-        Date::new("Italian Resturant".into()),
-        Date::new("Sexy Times".into()),
+        // Date::new("Italian Resturant".into()),
+        // Date::new("Sexy Times".into()),
     ]))));
     let config = move |cfg: &mut ServiceConfig| {
         cfg.service(
