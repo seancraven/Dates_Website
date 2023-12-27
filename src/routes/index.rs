72ef3d19a52f4fe50ea1e653c5a252b10a4a96b7
sean craven <seanpietercraven@gmail.com>
2023-12-27 23:24:19 +0000

    wip: working on making a new user endpoint.

--- src/routes/index.rs
+++ src/routes/index.rs
@@ -1,12 +1,14 @@
 use std::fs;
 
+use crate::auth::user::UnauthorizedUser;
 use crate::domain::repository::{AppState, MissingUserError};
 use crate::domain::{dates::Date, repository::ExpansionCache};
 use crate::routes::dates_service::render_dates;
+use actix_web::web::Form;
 use actix_web::{get, web::Data, web::Path, HttpResponse, Responder};
 use anyhow::anyhow;
-use log::debug;
 use tera::{Context, Tera};
+use tracing::{debug, error, info};
 use uuid::Uuid;
 
 pub fn unauthorized() -> HttpResponse {
@@ -16,7 +18,7 @@ pub fn unauthorized() -> HttpResponse {
 #[get("/{user_id}")]
 pub async fn index(app_state: Data<AppState>, user_id: Path<Uuid>) -> impl Responder {
     if !app_state.repo.check_user_has_access(&user_id).await {
-        log::info!("Unauthorized user {:?} attempted access", user_id);
+        info!("Unauthorized user {:?} attempted access", user_id);
         return unauthorized();
     }
     if app_state.cache.reset(&user_id).is_err() {
@@ -46,7 +48,24 @@ fn template_load(
 async fn landing() -> impl Responder {
     HttpResponse::Ok().body(fs::read_to_string("./pages/landing.html").unwrap())
 }
+// TODO: This is a dummy version of the login page.
+#[get("/login")]
+async fn dummy_login() -> impl Responder {
+    HttpResponse::Ok().body(fs::read_to_string("./pages/dummy_login.html").unwrap())
+}
 #[get("/googleb0081feae6701197.html")]
 async fn search_verification() -> impl Responder {
     HttpResponse::Ok().body(fs::read_to_string("./pages/googleb0081feae6701197.html").unwrap())
 }
+
+// #[get("/login/get_new_user")]
+// async fn create_user(app_state: Data<AppState>, user_info: Form<UnauthorizedUser>) -> HttpResponse {
+//     let Ok(user) = user_info
+//         .into_inner()
+//         .create_user_and_group(app_state)
+//         .await
+//         .map_err(|e| {
+//             error!("{:?}", e);
+//             e
+//         });
+// }
