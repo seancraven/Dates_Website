584fd1eeb1dbb6218662bac7de25efaaddd3412d
seancraven <seanpietercraven@gmail.com>
2024-01-20 10:18:26 +0000

    feat: docs

--- src/auth/user.rs
+++ src/auth/user.rs
@@ -8,10 +8,19 @@ use uuid::Uuid;
 #[async_trait]
 #[allow(clippy::module_name_repetitions)]
 pub trait UserRepository {
+    /// Create a user and a group.
+    ///
+    /// Primary method for making a new user and founding a group.
+    ///
+    /// * `user`: New user, that doesn't have a target group to join.
     async fn create_user_and_group(&self, user: NoGroupUser) -> anyhow::Result<GroupUser> {
         self.add_user_to_group(user, self.create_group().await?)
             .await
     }
+    /// Create a user and add them to an existing group.
+    ///
+    /// * `user`: New user without a group.
+    /// * `email`: email of a member of the target group.
     async fn create_user_and_join_by_email(
         &self,
         user: NoGroupUser,
@@ -21,14 +30,26 @@ pub trait UserRepository {
             .await
     }
     // To implememnt
+    /// Add a user to a group via group id.
     async fn add_user_to_group(&self, user: NoGroupUser, group: i32) -> anyhow::Result<GroupUser>;
+    /// Create a new group.
     async fn create_group(&self) -> anyhow::Result<i32>;
+    /// Find a group by the email of a member.
+    ///
+    /// * `email`: Group member's email.
     async fn get_group_by_email(&self, email: &str) -> anyhow::Result<i32>;
+    /// Validate a user.
+    ///
+    /// * `user`:
     async fn validate_user(
         &self,
         user: &UnauthorizedUser,
     ) -> Result<AuthorizedUser, UserValidationError>;
+    /// Add a user to the repository. With their selected password.
+    ///
+    /// * `user`: A user without an account.
     async fn create_authorized_user(&self, user: UnauthorizedUser) -> anyhow::Result<NoGroupUser>;
+    /// Update an existing authorized users's password.
     async fn change_user_password(
         &self,
         user: AuthorizedUser,
